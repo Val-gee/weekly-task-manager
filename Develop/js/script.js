436db5d3a8f8c63e1a74be5f0af5bb09caff202d
val.gee <vguevara0806@gmail.com>
2022-12-08 10:19:55 -0500

    added mdb features to HTML

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,11 +1,14 @@
 var APIKey = "87e0914af1422e36fd0e0a7774720d6f";
+
 //today's date
 var today = dayjs();
 var todayFormatted = today.format("dddd, MMM D");
 var currentDate = document.getElementById("currentDate");
+
 //today's time
 var time = today.format("h:mm a");
 var currentTime = document.getElementById("currentTime");
+
 //timer button
 var timerButton = document.querySelector(".timer");
 
